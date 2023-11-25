63a44973ed83d9bc2cf0a5d0480598fbd5de4d48
Philippe Rey <151957162+F4lseDev@users.noreply.github.com>
2023-11-25 20:20:34 +0100

    Added func´s + added comments to .env sample
    + get_voucher
    + get_network_details

--- main.py
+++ main.py
@@ -54,30 +54,86 @@ def get_network(DEFAULT_URL, Access_token, appID, appSecret):
     payload = "&".join([f"{key}={payload_data[key]}" for key in payload_data])
     
     network_url = f'{DEFAULT_URL}/oapi/v1.0.0/network/list'
-    network_response = requests.post(network_url + "?" + payload, data=body, headers={'Content-type': 'application/json'}, verify=False, timeout=30)
+    network_response = requests.get(network_url + "?" + payload, data=body, headers={'Content-type': 'application/json'}, timeout=30)
+    debug(network_response.url)
+    debug(ljson(network_response.text)['data'])
 
-    debug(ljson(network_response.text))
-    
-    
-def get_voucher(DEFAULT_URL, Access_token, appID, networkID):
-    param = {
-     "search": "",
-     "order": "",
-     "pageNum": 1,
-     "pageSize": 10,
-     "networkId": networkID
+def get_network_details(DEFAULT_URL, Access_token, appID, appSecret, networkID):
+    timestamp = round(time.time() * 1000)
+    public_params = {
+        'access_token': Access_token,
+        'appID': appID,
+        'secretKey': appSecret,
+        'timestamp': timestamp
+    }
+        
+    body_data = {
+        "id": networkID
     }
     
-    signatur, timestamp = create_signature(Access_token=Access_token, appID=appID, param=param)
-    URL = DEFAULT_URL + f"oapi/v1.0.0/voucher/list?&appID={appID}&timestamp={timestamp}&signatur={signatur}"
-    headers = { 'Content-type': 'application/json'}
-    debug(URL)
-    r = requests.post(url=URL, params=param,  headers=headers)
-    debug(r.json)
+    params = "&".join([f"{key}={public_params[key]}" for key in public_params])
+    body = json.dumps(body_data, separators=(',', ':'))
+
+    body_signature = sha256(body.encode()).hexdigest()
+    signature = sha256(f"&{params}&{body_signature}&".encode()).hexdigest()
+    
+    payload_data = {
+        'access_token': Access_token,
+        'appID': appID,
+        'timestamp': timestamp,
+        'signature': signature
+    }
+
+    payload = "&".join([f"{key}={payload_data[key]}" for key in payload_data])
+    
+    network_url = f'{DEFAULT_URL}/oapi/v1.0.0/network/detail'  # Updated endpoint
+    network_response = requests.post(network_url + "?" + payload, data=body, headers={'Content-type': 'application/json'}, timeout=30)
+
+    debug(ljson(network_response.text)['data'])
+
+
+
+    
+def get_voucher(DEFAULT_URL, Access_token, appID, appSecret, networkID):
+    timestamp = round(time.time() * 1000)
+    public_params = {
+        'access_token': Access_token,
+        'appID': appID,
+        'secretKey': appSecret,
+        'timestamp': timestamp
+    }
+        
+    body_data = {
+        "search": "",
+        "order": "name",
+        "pageNum": 1,
+        "pageSize": 10,
+        "networkId": networkID
+    }
+    params = "&".join([f"{key}={public_params[key]}" for key in public_params])
+    body = json.dumps(body_data, separators=(',', ':'))
+
+    body_signature = sha256(body.encode()).hexdigest()
+    signature = sha256(f"&{params}&{body_signature}&".encode()).hexdigest()
+    
+    payload_data = {
+        'access_token': Access_token,
+        'appID': appID,
+        'timestamp': timestamp,
+        'signature': signature
+    }
+
+    payload = "&".join([f"{key}={payload_data[key]}" for key in payload_data])
+    
+    network_url = f'{DEFAULT_URL}/oapi/v1.0.0/voucher/list'
+    network_response = requests.post(network_url + "?" + payload, data=body, headers={'Content-type': 'application/json'}, timeout=30)
+
+    debug(ljson(network_response.text)['data'])
     
     
+ 
 def ljson(input):
-    json_return = json.loads(input)
+    json_return = json.loads(str(input))
     return json_return
 
 
@@ -88,3 +144,8 @@ def debug(input):
 key = get_token(DEFAULT_URL=DEFAULT_ENV, ID=ID_ENV, SECRET_KEY=SECRET_KEY_ENV)
 
 get_network(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV)
+
+
+get_network_details(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV, networkID=1)
+
+get_voucher(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV, networkID=1)
