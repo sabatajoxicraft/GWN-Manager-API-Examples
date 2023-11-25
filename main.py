3cfaf9ee64333eee35cefa7297297ea11030a144
Philippe Rey <151957162+F4lseDev@users.noreply.github.com>
2023-11-25 21:37:01 +0100

    Added get_ssid
    Returns a list of ssids with infos

--- main.py
+++ main.py
@@ -130,7 +130,41 @@ def get_voucher(DEFAULT_URL, Access_token, appID, appSecret, networkID):
 
     debug(ljson(network_response.text)['data'])
     
+def get_ssid(DEFAULT_URL, Access_token, appID, appSecret, networkID):
+    timestamp = round(time.time() * 1000)
+    public_params = {
+        'access_token': Access_token,
+        'appID': appID,
+        'secretKey': appSecret,
+        'timestamp': timestamp
+    }
+        
+    body_data = {
+        "search": "",
+        "order": "name",
+        "pageNum": 1,
+        "pageSize": 10,
+        "networkId": networkID
+    }
+    params = "&".join([f"{key}={public_params[key]}" for key in public_params])
+    body = json.dumps(body_data, separators=(',', ':'))
+
+    body_signature = sha256(body.encode()).hexdigest()
+    signature = sha256(f"&{params}&{body_signature}&".encode()).hexdigest()
     
+    payload_data = {
+        'access_token': Access_token,
+        'appID': appID,
+        'timestamp': timestamp,
+        'signature': signature
+    }
+
+    payload = "&".join([f"{key}={payload_data[key]}" for key in payload_data])
+    
+    network_url = f'{DEFAULT_URL}/oapi/v1.0.0/ssid/list'
+    network_response = requests.post(network_url + "?" + payload, data=body, headers={'Content-type': 'application/json'}, timeout=30)
+
+    debug(ljson(network_response.text)['data'])
  
 def ljson(input):
     json_return = json.loads(str(input))
@@ -145,7 +179,8 @@ key = get_token(DEFAULT_URL=DEFAULT_ENV, ID=ID_ENV, SECRET_KEY=SECRET_KEY_ENV)
 
 get_network(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV)
 
-
 get_network_details(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV, networkID=1)
 
 get_voucher(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV, networkID=1)
+
+get_ssid(DEFAULT_URL=DEFAULT_ENV, Access_token=key, appID=ID_ENV, appSecret=SECRET_KEY_ENV, networkID=1)
